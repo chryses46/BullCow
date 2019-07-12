8b17e562765cdee9be4a2ad7fcb302dbce5fea6b
Daniel Frank <danjfrank08@gmail.com>
2019-07-12 16:50:58 -0500

    Playing with HiddenWord
    HiddenWord interaction

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -1,12 +1,31 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
 
+FString HiddenWord = TEXT("telemetry");
+
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
-    Super::BeginPlay();
+    Super::BeginPlay(); 
+    PrintLine(TEXT("Welcome to Bulls & Cows."));
+    PrintLine(FString::Printf(TEXT("Guess the %i letter isogram!"), HiddenWord.Len()));
+
+
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
+    // ClearScreen();
 
+    if(Input.Len() == HiddenWord.Len())
+    {
+        PrintLine(TEXT("Yep. That's a four letter word."));
+    }
+    else if(Input == HiddenWord)
+    {
+        PrintLine(TEXT("You guessed the right word! Great work."));
+    }
+    else
+    {
+        PrintLine(FString::Printf(TEXT("Enter a %i letter word..."), HiddenWord.Len()));
+    }
 }
